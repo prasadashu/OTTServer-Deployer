8be8b021818afbcaaa8451dec83866551105f77b
Ashu <ashuprasad29516@gmail.com>
2021-10-07 01:54:45 +0530

    Removed the original non-jinja file

--- roles/setup_scripts/templates/path.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $path_movies = "path_movies.txt";
-    $path_tv = "path_tv.txt";
-    $path_cached_images = "path_cached_images.txt";
-    $path_cached_images_ip = "cached_images/";
-    $path_server = "/";
-?>
